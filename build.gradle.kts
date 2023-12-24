e2b6e17bf95933c71ea2d4da80870139b1c16498
Ian Emmons <ian@emmons.mobi>
2023-12-23 20:58:05 -0500

    Added fat and cross-platform jar support

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,19 @@ repositories {
 	mavenCentral()
 }
 
+val javaFxVersion = "21.0.1"
+
 dependencies {
 	implementation("org.apache.commons:commons-lang3:3.14.0")
-	//implementation("org.apache.commons:commons-text:1.11.0")
 	implementation("com.google.code.gson:gson:2.10.1")
-	implementation("org.apache.poi:poi-ooxml:5.2.4")
+	implementation("org.apache.poi:poi-ooxml:5.2.5")
 
 	// This forces POI's log4j dependency to the latest 2.x version:
 	implementation("org.apache.logging.log4j:log4j-core:2.22.0")
+
+	runtimeOnly("org.openjfx:javafx-graphics:$javaFxVersion:win")
+	runtimeOnly("org.openjfx:javafx-graphics:$javaFxVersion:linux")
+	runtimeOnly("org.openjfx:javafx-graphics:$javaFxVersion:mac")
 }
 
 java {
@@ -23,19 +28,28 @@ java {
 	}
 }
 
-application {
-	mainClass.set("org.virginiaso.score_scope_export.gui.ExportApplication")
+tasks.jar {
+	manifest.attributes["Main-Class"] = "org.virginiaso.score_scope_export.gui.Launcher"
+	from(configurations
+		.runtimeClasspath
+		.get()
+		.map { zipTree(it) })
+	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 javafx {
-	version = "21.0.1"
-	modules("javafx.controls" /*, "javafx.fxml" */)
+	version = "$javaFxVersion"
+	modules("javafx.controls")
+}
+
+application {
+	mainClass.set("org.virginiaso.score_scope_export.gui.ExportApplication")
 }
 
 testing {
 	suites {
 		val test by getting(JvmTestSuite::class) {
-			useJUnitJupiter("5.9.3")
+			useJUnitJupiter("5.10.1")
 		}
 	}
 }
---
+++ src/main/java/org/virginiaso/score_scope_export/gui/Launcher.java
@@ -0,0 +1,7 @@
+package org.virginiaso.score_scope_export.gui;
+
+public class Launcher {
+	public static void main(String[] args) {
+		ExportApplication.main(args);
+	}
+}
